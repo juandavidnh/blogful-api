5e8d9ce95a74c40995e846d69158d8aeed4f15d4
Juan David Nunez <juandavidnh93@gmail.com>
2020-01-19 19:43:29 -0500

    refactored POST and DELETE

--- test/articles.fixtures.js
+++ test/articles.fixtures.js
@@ -31,6 +31,27 @@ function makeArticlesArray() {
     ];
 }
 
+function makeMaliciousArticle() {
+    const maliciousArticle = {
+        id: 911,
+        style: 'How-to',
+        date_published: new Date().toISOString(),
+        title: 'Naughty <script>alert("xss");</script>',
+        content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+    }
+    const expectedArticle = {
+        ...maliciousArticle,
+        title: 'Naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+        content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
+    }
+
+    return {
+        maliciousArticle,
+        expectedArticle,
+    }
+}
+
 module.exports = {
     makeArticlesArray,
+    makeMaliciousArticle,
 }
